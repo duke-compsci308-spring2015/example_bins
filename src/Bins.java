e0a5057df565bf41076166ea63d6413435d3349a
Robert C. Duvall <robertcduvall@gmail.com>
2015-01-14 19:51:04 -0500

    Refactored to encapsulate use of PriorityQueue

--- src/Bins.java
+++ src/Bins.java
@@ -1,20 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-
 /**
  * Runs a number of algorithms that try to fit files onto disks.
  */
-public class Bins
-{
+public class Bins {
     public static final String WORST_FIT = "worst-fit";
     public static final String WORST_FIT_DECREASING = "worst-fit decreasing";
-
 
     /**
      * Reads list of integer data from the given input.
@@ -22,11 +20,9 @@ public class Bins
      * @param input tied to an input source that contains space separated numbers
      * @return list of the numbers in the order they were read
      */
-    public List<Integer> readData (Scanner input)
-    {
-        List<Integer> results = new ArrayList<Integer>();
-        while (input.hasNext())
-        {
+    public List<Integer> readData (Scanner input) {
+        List<Integer> results = new ArrayList<>();
+        while (input.hasNext()) {
             results.add(input.nextInt());
         }
         return results;
@@ -35,79 +31,69 @@ public class Bins
     /**
      * Returns sum of all values in given list.
      */
-    public int getTotal (List<Integer> data)
-    {
+    public int getTotal (Collection<Integer> data) {
         int total = 0;
-        for (int d : data)
-        {
+        for (int d : data) {
             total += d;
         }
         return total;
     }
 
     // add files to the collection of Disks
-    private void addFiles (List<Integer> data, PriorityQueue<Disk> pq)
-    {
+    private Collection<Disk> addFiles (List<Integer> data) {
+        PriorityQueue<Disk> pq = new PriorityQueue<>();
         pq.add(new Disk(0));
         int diskId = 1;
-        for (Integer size : data)
-        {
+        for (Integer size : data) {
             Disk d = pq.peek();
-            if (d != null && d.freeSpace() >= size)
-            {
+            if (d != null && d.freeSpace() >= size) {
                 pq.poll();
                 d.add(size);
                 pq.add(d);
             }
-            else
-            {
+            else {
                 Disk d2 = new Disk(diskId);
                 diskId++;
                 d2.add(size);
                 pq.add(d2);
             }
         }
+        return pq;
     }
 
     // print contents of given pq and a header, description
-    private void printResults (PriorityQueue<Disk> pq, String description)
-    {
+    private void printResults (String description, Collection<Disk> disks) {
         System.out.println();
         System.out.println("\n" + description + " method");
-        System.out.println("number of disks used: " + pq.size());
-        PriorityQueue<Disk> copy = new PriorityQueue<>(pq);
-        while (!copy.isEmpty())
-        {
-            System.out.println(copy.poll());
+        System.out.println("number of disks used: " + disks.size());
+        for (Disk d : disks) {
+            System.out.println(d);
         }
     }
 
-    // add contents of given data to pq and print results
-    private void runAlgorithm (List<Integer> data, PriorityQueue<Disk> pq, String description)
-    {
-        addFiles(data, pq);
-        printResults(pq, description);
+    // try a worst fit algorithm and also show the results
+    private void runAlgorithm (List<Integer> data, String description) {
+        List<Integer> copy = new ArrayList<>(data);
+        if (description.equals(WORST_FIT_DECREASING)) {
+            Collections.sort(copy, Collections.reverseOrder());
+        }
+        Collection<Disk> disks = addFiles(data);
+        printResults(description, disks);
     }
 
     /**
      * The main program.
      */
-    public static void main (String args[])
-    {
+    public static void main (String args[]) {
         Bins b = new Bins();
-        try
-        {
+        try {
             Scanner input = new Scanner(new File(args[0]));
             List<Integer> data = b.readData(input);
             System.out.println("total size = " + b.getTotal(data) / 1000000.0 + "GB");
-
-            PriorityQueue<Disk> pq = new PriorityQueue<>();
-            b.runAlgorithm(data, pq, WORST_FIT);
-            Collections.sort(data, Collections.reverseOrder());
-            b.runAlgorithm(data, pq, WORST_FIT_DECREASING);
+            b.runAlgorithm(data, WORST_FIT);
+            b.runAlgorithm(data, WORST_FIT_DECREASING);
         }
-        catch (FileNotFoundException e)
-        {
+        catch (FileNotFoundException e) {
             System.err.println("Could not open file: " + args[0]);
         }
     }
