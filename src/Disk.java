75e087af0fdc2cf433a3dfce75cf8a8226532839
Robert C. Duvall <robertcduvall@gmail.com>
2015-02-05 12:25:59 -0500

    Refactored Disk to keep track of its own ID using a class (static) variable

--- src/Disk.java
+++ src/Disk.java
@@ -5,28 +5,28 @@ import java.util.List;
 /**
  * Represents a collection of files; how many it can hold is limited by its capacity.
  */
-public class Disk implements Comparable<Disk>
-{
+public class Disk implements Comparable<Disk> {
     public static final int MAX_CAPACITY = 1000000;
+    private static int ourId = 0;
     private int myId;
     private int mySize;
     private int myCapacity;
     private List<Integer> myFiles;
 
+
+    /**
+     * Reset Disk's ID numbering algorithm.
+     */
+    public static void reset () {
+        ourId = 0;
+    }
+
+
     /**
      * Create an empty Disk.
      */
-    public Disk ()
-    {
-        this(0);
-    }
-
-    /**
-     * Create an empty Disk with the given ID.
-     */
-    public Disk (int id)
-    {
-        myId = id;
+    public Disk () {
+        myId = ourId++;
         mySize = 0;
         myCapacity = MAX_CAPACITY;
         myFiles = new ArrayList<Integer>();
@@ -35,20 +35,18 @@ public class Disk implements Comparable<Disk>
     /**
      * @return amount of free space available on this disk
      */
-    public int freeSpace ()
-    {
+    public int freeSpace () {
         return myCapacity - mySize;
     }
 
     /**
      * Adds the given file to this disk.
-     * 
+     *
      * No error checking is done to ensure the file can fit on this disk.
-     * 
+     *
      * @param filesize size of file to add to this disk
      */
-    public void add (int filesize)
-    {
+    public void add (int filesize) {
         myFiles.add(filesize);
         mySize += filesize;
     }
@@ -57,11 +55,9 @@ public class Disk implements Comparable<Disk>
      * Converts this disk's information to a string.
      */
     @Override
-    public String toString ()
-    {
+    public String toString () {
         String result = myId + "\t" + freeSpace() + ":\t";
-        for (int k = 0; k < myFiles.size(); k++)
-        {
+        for (int k = 0; k < myFiles.size(); k++) {
             result += " " + myFiles.get(k);
         }
         return result;
@@ -69,41 +65,39 @@ public class Disk implements Comparable<Disk>
 
     /**
      * Check if this disk's information is the same as the given disk's.
-     * 
+     *
      * Note, currently only equality is based only on their ID.
-     * 
+     *
      * @param other disk to compare to this disk
      * @return true iff their values match
      */
     @Override
-    public boolean equals (Object other)
-    {
-        return other != null && other instanceof Disk && 
-               myId == ((Disk)other).myId;
+    public boolean equals (Object other) {
+        return other != null && other instanceof Disk && myId == ((Disk) other).myId;
     }
 
     /**
      * Compare this disk's information to the given disk's for ordering.
-     * 
+     *
      * Note, currently only equality is based only on their free space.
-     * 
+     *
      * @param other disk to compare to this disk
-     * @return positive if this disk is greater than the given disk,
-     *         zero if they are equal, and
+     * @return positive if this disk is greater than the given disk, zero if they are equal, and
      *         negative if this disk is less than the given disk
      */
     @Override
-    public int compareTo (Disk other)
-    {
-        if (other != null)
-        {
+    public int compareTo (Disk other) {
+        if (other != null) {
             int result = other.freeSpace() - freeSpace();
-            if (result == 0)
+            if (result == 0) {
                 return myId - other.myId;
-            else 
+            }
+            else {
                 return result;
+            }
         }
-        else 
+        else {
             return -1;
+        }
     }
 }
